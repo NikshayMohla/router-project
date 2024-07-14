0d0df04f732d908c9f64c0a3058d7d07799a1832
NikshayMohla <78416149+NikshayMohla@users.noreply.github.com>
2024-07-14 10:50:37 +0530

    Login Form

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
+
+
+const LoginForm = () => {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const changeHandler = (e) => {
+        setFormData((prevData) => ({
+            ...prevData,
+            [e.target.name]: e.target.value
+        }))
+    }
+    const [showPassword, setshowPassword] = useState(false)
+    return (
+        <form action="">
+            <label htmlFor="">
+                <p>Email Address<sup>*</sup></p>
+                <input required type="text" value={formData.email}
+                    onChange={changeHandler}
+                    placeholder='random@random.com' name='email' />
+            </label>
+            <label htmlFor="">
+                <p>Email Address<sup>*</sup></p>
+                <input required type={showPassword ? ("text") : ("passwords")} value={formData.password} onChange={changeHandler} placeholder='Enter Your Password' name='password' />
+
+                <span onClick={() => setshowPassword((prev) => !prev)}>
+                    showPassword?(<AiOutlineEyeInvisible />):(<AiOutlineEye />)
+                </span>
+                <Link to="">
+                    <p>Forgot Password</p>
+                </Link>
+            </label>
+
+            <button>Sign In</button>
+
+        </form>
+    )
+}
+
+export default LoginForm
---
+++ src/components/Template.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import frameImage from "../assets/frame.png"
+const Template = ({ title, desc1, desc2, image, formtype, setIsLoggedIn }) => {
+    return (
+        <div>
+            <div>
+                <h1>{title}</h1>
+                <p>
+                    <span>{desc1}</span>
+                    <span>{desc2}</span>
+                </p>
+                {formtype == "signup"}?(<SignUpForm></SignUpForm>):(<LoginForm />)
+                <div>
+                    <div></div>
+                    <p>OR</p>
+                </div>
+                <button>Sign Up With Google</button>
+            </div>
+            <div>
+                <img src={frameImage} alt="Frame" width={558} height={504} loading='lazy' />
+                <img src={image} alt="student" width={558} height={490} loading='lazy' />
+            </div>
+        </div>
+    )
+}
+
+export default Template
